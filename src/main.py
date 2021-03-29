5f3a6e36f38051e146ff91cd6567f0e52928ad34
uiamn <33373522+uiamn@users.noreply.github.com>
2021-03-30 01:34:24 +0900

    detect 一 (meaning "one" in kanji) as a long vowel

--- src/main.py
+++ src/main.py
@@ -10,7 +10,7 @@ from is_conform import is_conform
 # global variables
 DEBUG = False
 LTI_FILEPATH = os.path.join(os.path.dirname(__file__), 'LATEST_TWEET_ID')
-LONG_VOWELS = re.compile(r'[ー〜]')
+LONG_VOWELS = re.compile(r'[ー一〜]')
 
 # MeCab
 mcb = MeCab.Tagger(
